2417980f03e88e12c042968d85c1b9e4eacafa59
JUAN JOSE ALZATE GARCIA <juan_alzate82212@elpoli.edu.co>
2026-01-06 17:12:19 -0500

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Remove properties that do not have any decorators
@@ -12,7 +16,7 @@ async function bootstrap() {
       transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
     }),
   );
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
